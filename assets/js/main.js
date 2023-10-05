41060854e4ea8e4e106986dfd96e987fd24accb6
front-dv <zhayitovv@gmail.com>
2023-10-05 16:19:02 +0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHiddenPass = (loginPass, loginEye) =>{
+    const input = document.getElementById(loginPass),
+          iconEye = document.getElementById(loginEye)
+    iconEye.addEventListener('click', ()=>{
+        // Change password to text
+        if(input.type === 'password'){
+            	// Switch to text
+                input.type = 'text'
+
+                	// Icon change
+                    iconEye.classList.add('ri-eye-line')
+                    iconEye.classList.remove('ri-eye-off-line')
+        }else{
+            // Change to password
+            input.type = 'password'
+            iconEye.classList.remove('ri-eye-line')
+            iconEye.classList.add('ri-eye-off-line')  
+        }
+    })      
+}
+ 
+showHiddenPass('login-pass','login-eye')
